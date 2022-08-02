3e4c0a2b22a5647bd52c6e947b0ba77c2b3130c0
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-08-03 03:35:08 +0900

    Feat: Print hint when there is no cell for today

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
+require 'date'
 require 'mechanize'
 
+today = Date.today
 agent = Mechanize.new
 page = agent.get("https://github.com/#{ENV.fetch('TARGET_GITHUB_USERNAME')}")
-count = page.search("//rect[@data-date='#{Date.today}']").first.attributes['data-count'].value
-exit 1 if count.nil?
+cell = page.search("//rect[@data-date='#{today}']").first
+unless cell # It's UTC based so sometimes there's no cell for today
+  puts "The cell for today doesn't exist, run it again after #{Time.utc(today.year, today.month, today.day).localtime}"
+  exit 0
+end
+
+count = cell.attributes['data-count'].value
+exit 1 if count.nil? # Fatal, something is wrong
 
 require 'slack-notifier'
 
